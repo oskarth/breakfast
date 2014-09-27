1dd4f2e604be9e6f616dfe885df7db638db24ab1
oskarth <me@oskarth.com>
2014-09-27 05:05:14 +0200

    irc on server-side (incoming/send msgs)

--- src/clj/breakfast/server.clj
+++ src/clj/breakfast/server.clj
@@ -6,9 +6,30 @@
             [net.cgrand.enlive-html :as html :refer (deftemplate)]
             [environ.core :refer (env)]
             [cemerick.piggieback :as piggieback]
-            [weasel.repl.websocket :as weasel]))
+            [weasel.repl.websocket :as weasel]
+            [irclj.core :as irc]))
 
+;; IRC stuff
 
+(defn handle-incoming
+  "Deal with incoming IRC messages."
+  [text nick]
+  (prn (str "INCOMING: " text " (" nick ")")))
+
+(def connection
+  "IRC connection."
+  (irc/connect "irc.freenode.net" 6667 "breakfastbot"
+               :callbacks {:privmsg (fn [_ {:keys [text nick]}]
+                                      (handle-incoming text nick))}))
+
+;; automatically join channel at startup
+(irc/join connection "#clojurecup-breakfast")
+
+(defn message
+  "Send a message through IRC."
+  [s]
+  (irc/message connection "#clojurecup-breakfast" (str "foo says: " s)))
+  
 (defn body-transforms []
   (if (env :is-dev)
     (comp
