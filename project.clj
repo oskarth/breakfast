79f161f8d8b7ae2dbe77af71e7ef5ece7c59eaa1
oskarth <me@oskarth.com>
2014-09-28 23:07:37 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject breakfast "0.1.0-SNAPSHOT"
+(defproject breakfast "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
